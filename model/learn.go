f7fa8fd3590f4598e58a014d5bb590a9c4d5917a
KuJyoAi <kujyo.eia.serias@gmail.com>
2024-03-09 13:49:16 +0800

    add: dir

--- model/learn.go
+++ model/learn.go
@@ -1,6 +1,8 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 const (
 	QuestionsTypeElementary = "element"
@@ -10,10 +12,41 @@ const (
 
 type Question struct {
 	gorm.Model
-	Type   string `gorm:"type" json:"type"`
-	Answer string `gorm:"answer" json:"answer"`
-	Src    string `gorm:"src" json:"src"`   // 资源文件
-	Desc   string `gorm:"desc" json:"desc"` // 描述
+	Type string `gorm:"type:varchar(20)" json:"type"`
+	//Data          json.RawMessage `gorm:"type:json" json:"data"`
+	// 直接从目录下找文件
+	Answer        string `json:"answer"`         // 答案
+	ConfuseAnswer string `json:"confuse_answer"` // 错误答案
+}
+
+func (Question) TableName() string {
+	return "questions"
+}
+
+type QuestionElement struct {
+	Name              string `json:"name"`               // element name
+	Desc              string `json:"desc"`               // element desc
+	SpellPicPath      string `json:"spell_pic_path"`     // spell pic
+	LipPicPath        string `json:"lip_pic_path"`       // lip pic
+	LongSentencePath  string `json:"long_sentence_path"` // one long sentence
+	ShortSentencePath string `json:"short_sentence"`     // one short sentence
+}
+
+type QuestionCharacter struct {
+	Name         string `json:"name"`           // character name 你
+	Spell        string `json:"spell"`          // character spell nǐ
+	VoicePath    string `json:"voice_path"`     // character voice
+	Desc         string `json:"desc"`           // character desc
+	SpellPicPath string `json:"spell_pic_path"` // spell pic
+	LipPicPath   string `json:"lip_pic_path"`   // lip pic
+	VideoPath    string `json:"video_path"`     // video
+}
+
+type QuestionWord struct {
+	Name      string `json:"name"`       // word name
+	Spell     string `json:"spell"`      // word spell
+	VoicePath string `json:"voice_path"` // word voice
+	VideoPath string `json:"video_path"` // word video
 }
 
 type UserLearnRecord struct {
